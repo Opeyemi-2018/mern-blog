1e7ee7efb3d7f5597d937c10a334d4dbb1b7c818
opeyemi <opeyemiodunayo2018@gmail.com>
2024-07-28 14:28:41 -0700

    create env

--- server/index.js
+++ server/index.js
@@ -1,5 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 let app = express()
 
-app.listen(3000, ()=> console.log('port i running on port 3000'))
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+      app.listen(3000, ()=> console.log('connected to database'))  })
+  .catch((err) => console.log(err))
+
