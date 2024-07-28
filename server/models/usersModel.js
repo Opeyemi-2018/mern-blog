1fb731dff83d4a11e3f70ea175fc5d4dad6f8a57
opeyemi <opeyemiodunayo2018@gmail.com>
2024-07-28 14:51:07 -0700

    user model created

---
+++ server/models/usersModel.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+let userSchema = mongoose.Schema({
+    username: {type: String, require: true, unique: true},
+    email: {type: String, require: true, unique: true},
+    password: {type: String, require: true}
+}, {timestamp: true}) 
+
+export let User = mongoose.model('User', userSchema)
